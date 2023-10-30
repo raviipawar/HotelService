94f1d9ee6fb1efd2edf358807ee2ea18a906bf53
raviipawar <ravibalajipawar@gmail.com>
2023-10-30 18:34:24 +0530

    Controller and pom changes

--- src/main/java/com/learning/hotelservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/learning/hotelservice/controller/CurrencyExchangeController.java
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.learning.hotelservice.entities.ExchangeValue;
 import com.learning.hotelservice.services.CurrencyExchangeService;
 
 @RestController
-@RequestMapping("/currencyExchange")
 public class CurrencyExchangeController {
 
 	@Autowired
